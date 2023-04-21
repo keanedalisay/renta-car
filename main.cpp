94d2c75c1c8f1c5ed5c4a5c8e4afceda599880a3
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-04-21 14:29:46 +0800

    Add home function to act as main dashboard

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <windows.h>
 #include <unistd.h>
 
+void Home(void);
+
 class Welcome
 {
 public:
@@ -43,4 +45,54 @@ int main()
 
     Welcome node;
     node.output();
+    Home();
+}
+
+void Home(void)
+{
+    using std::cout;
+    using std::setw;
+
+    char choice;
+
+    cout << setw(86) << "++================++++===============++ \n";
+    cout << setw(86) << "||                                   || \n";
+    cout << setw(86) << "||   CAR RENTAL SERVICES  by         || \n";
+    cout << setw(86) << "||                      RentA-Car    || \n";
+    cout << setw(86) << "||                                   || \n";
+    cout << setw(86) << "++================++++===============++ \n";
+    cout << "\n";
+    cout << setw(92) << "Proceed to our Car Rental Form by entering [Y]... \n";
+    cout << setw(85) << "Otherwise, enter [N] to exit program...";
+    cout << "\n\n";
+    cout << setw(65) << ": ";
+    std::cin >> choice;
+    choice = toupper(choice);
+    std::cin.ignore(1000, '\n');
+
+    if (choice == 'Y')
+    {
+        cout << "\n"
+             << setw(78) << "Great, Let's get started!\n";
+        sleep(2);
+        system("cls");
+    }
+    else if (choice == 'N')
+    {
+        cout << "\n\n"
+             << setw(85) << "** Thank you for your consideration. **";
+        exit(0);
+    }
+    else
+    {
+        cout << "\n"
+             << setw(82) << "Invalid Choice. Please Try Again.";
+        sleep(1);
+        cout << "\n"
+             << setw(76) << "Restarting Program...";
+        sleep(2);
+        system("cls");
+        Home();
+    }
+    return;
 }
