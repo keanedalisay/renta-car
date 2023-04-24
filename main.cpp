75914e9f1cbfb84566efadebcaf56d1e6c4645a9
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-04-24 12:07:04 +0800

    Refactor welcome, home, and car rental form

--- main.cpp
+++ main.cpp
@@ -6,46 +6,81 @@
 #include <windows.h>
 #include <unistd.h>
 
+void Welcome(void);
 void Home(void);
 
-class Welcome
+class CarRentalForm
 {
-public:
-    int output()
+private:
+    std::string name;
+    int age;
+    int car_rent_days;
+
+    void hdng(void)
     {
-        std::ifstream in("renta-car.txt");
+        std::cout << "\n"
+                  << std::setw(90) << "           CAR RENTAL FORM         \n";
+        std::cout << std::setw(90) << "-----------------------------------\n";
+        std::cout << "\n"
+    };
 
-        if (!in)
-        {
-            std::cout << "Cannot open input file.\n";
-        }
-        char str[1000];
-        while (in)
-        {
-            in.getline(str, 1000);
-            if (in)
-                std::cout << str << "\n";
-        }
-        in.close();
+public:
+    void show(void)
+    {
+        using std::cout;
+        using std::setw;
 
-        sleep(1);
-        std::cout << "\nStarting the program please wait..."
-                  << "\n";
-        sleep(1);
-        std::cout << "\nLoading up files..."
-                  << "\n";
-        sleep(1);
+        hdng();
+
+        << setw(90) << "Kindly fill out the following details.";
+        cout << "\n\n"
+             << setw(60) << "Full Name: ";
+        std::getline(std::cin, name);
+        cout << "\n"
+             << setw(55) << "Age: ";
+        std::cin >> age;
+        cout << "\n"
+             << setw(83) << "Duration of Car Rental [# of Days]: ";
+        std::cin >> car_rent_days;
+
         system("cls");
-        return 0;
     }
 };
 
 int main()
 {
 
-    Welcome node;
-    node.output();
+    Welcome();
     Home();
+    CarRentalForm form;
+    form.show();
+}
+
+void Welcome(void)
+{
+    std::ifstream in("renta-car.txt");
+
+    if (!in)
+    {
+        std::cout << "Cannot open input file.\n";
+    }
+    char str[1000];
+    while (in)
+    {
+        in.getline(str, 1000);
+        if (in)
+            std::cout << str << "\n";
+    }
+    in.close();
+
+    sleep(1);
+    std::cout << "\nStarting the program please wait..."
+              << "\n";
+    sleep(1);
+    std::cout << "\nLoading up files..."
+              << "\n";
+    sleep(1);
+    system("cls");
 }
 
 void Home(void)
